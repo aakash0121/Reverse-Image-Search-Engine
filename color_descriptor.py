7842df045ed75d2312aefea7c18569c34cafa2de
Aakash Bindal <bindalaakash60@gmail.com>
2020-05-06 11:39:00 +0530

    major change

--- color_descriptor.py
+++ color_descriptor.py
@@ -1,38 +1,66 @@
-import numpy as np
 import cv2
+import numpy as np 
+from test import bins
 import imutils
 
-def histogram(image, bins, mask):
-		
-		hist = cv2.calcHist([image], [0, 1, 2], mask, bins, [0, 180, 0, 256, 0, 256])
-			
-		if imutils.is_cv2():
-			hist = cv2.normalize(hist).flatten()
-		else:
-			hist = cv2.normalize(hist, hist).flatten()
 
-		return hist
+def histogram(image, mask):
+    # extract a 3D color histogram from the masked region of the
+    # image, using the supplied number of bins per channel
+    hist = cv2.calcHist([image], [0, 1, 2], mask, bins,
+        [0, 180, 0, 256, 0, 256])
 
-def color_descriptor(image, bins):
-    features = []
+    # normalize the histogram if we are using OpenCV 2.4
+    if imutils.is_cv2():
+        hist = cv2.normalize(hist).flatten()
+
+    # otherwise handle for OpenCV 3+
+    else:
+        hist = cv2.normalize(hist, hist).flatten()
+    return hist
+
+def feature_extractor(query_image):
+    # convert RGB color space into HSV
     image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-	
-    (h, w) = image.shape[:2]
-    (cX, cY) = (int(w * 0.5), int(h * 0.5))
-    
-    segments = [(0, cX, 0, cY), (cX, w, 0, cY), (cX, w, cY, h), (0, cX, cY, h)]
+    # features list where we store all 5 regions feature
+    features = []
 
-    (axesX, axesY) = (int(w * 0.75) // 2, int(h * 0.75) // 2)
-    ellipMask = np.zeros(image.shape[:2], dtype = "uint8")
-    cv2.ellipse(ellipMask, (cX, cY), (axesX, axesY), 0, 0, 360, 255, -1)
+    # dimensions of image
+    h = image.shape[0]
+    w = image.shape[1]
 
-    for (startX, endX, startY, endY) in segments:
-        cornerMask = np.zeros(image.shape[:2], dtype = "uint8")
-        cv2.rectangle(cornerMask, (startX, startY), (endX, endY), 255, -1)
-        cornerMask = cv2.subtract(cornerMask, ellipMask)
-        hist = histogram(image, bins, cornerMask)
+    # find out the center of the image
+    (cx, cy) = (int(w*0.5), int(h*0.5))
+
+    # divide the image into 5 regions
+    # 1. top-left region
+    top_left = (0, cx, 0, cy)
+    # 2. top_right region
+    top_right = (cx, w, 0, cy)
+    # 3. bottom-left region
+    bottom_left = (0, cx, cy, h)
+    # 4. bottom-right region
+    bottom_right = (cx, w, cy, h)
+    # 5. middle elliptic region
+    (aX, aY) = (int(w * 0.75) // 2, int(h * 0.75) // 2)
+    ellipse_mask = np.zeros(image.shape[:2], dtype = "uint8")
+    cv2.ellipse(ellipse_mask, (cx, cy), (aX, aY), 0, 0, 360, 255, -1)
+
+    regions = (top_left, top_right, bottom_right, bottom_left)
+
+    # loop over the first four segments
+    for (sX, eX, sY, eY) in regions:
+        # construct a mask for each corner of the image
+        # subtracting the elliptical center from it
+        side_mask = np.zeros(image.shape[:2], dtype = "uint8")
+        cv2.rectangle(side_mask, (sX, sY), (eX, eY), 255, -1)
+        siede_mask = cv2.subtract(side_mask, ellipse_mask)
+
+        # extract a color histogram from the image, then update the features list
+        hist = histogram(image, side_mask)
         features.extend(hist)
-
-    hist = histogram(image, ellipMask)
+    
+    # extract a color histogram from the elliptical region and update the feature vector
+    hist = histogram(image, ellipse_mask)
     features.extend(hist)
     return features
---
+++ test.py
@@ -0,0 +1,22 @@
+import cv2
+import color_descriptor
+import searcher
+
+#stored features file of out database
+index_path = "src/index.csv"
+
+#query image
+query_image_path = "src/queries/1.png"
+
+#database with which results will be shown
+database_path = "src/dataset/"
+
+# number of bins for color descriptor (H, S, V) respectively
+# total bins = 8*12*3 = 288
+bins = (8, 12, 3)
+
+# loading query image
+query_image = cv2.imread(query_image_path)
+
+# features list of query image as per the 5 regions
+features = color_descriptor.feature_extractor()
