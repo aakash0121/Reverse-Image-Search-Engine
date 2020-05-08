8810ea23c0d7b5e7028bfe080b3e07cbc812dbd5
Aakash Bindal <bindalaakash60@gmail.com>
2020-05-08 15:48:58 +0530

    added Model.py

---
+++ Model.py
@@ -0,0 +1,32 @@
+from keras.utils import plot_model
+from keras.models import Model, load_model
+from keras.layers import Input, Dense, LSTM, Embedding, Dropout
+from keras.layers.merge import add
+
+# define the captioning model
+def define_model(vocab_size, max_length):
+    # features from the CNN model squeezed from 2048 to 256 nodes
+    inputs1 = Input(shape=(2048,))
+    fe1 = Dropout(0.5)(inputs1)
+    fe2 = Dense(256, activation='relu')(fe1)
+    
+    # LSTM sequence model
+    inputs2 = Input(shape=(max_length,))
+    se1 = Embedding(vocab_size, 256, mask_zero=True)(inputs2)
+    se2 = Dropout(0.5)(se1)
+    se3 = LSTM(256)(se2)
+
+    # Merging both models
+    decoder1 = add([fe2, se3])
+    decoder2 = Dense(256, activation='relu')(decoder1)
+    outputs = Dense(vocab_size, activation='softmax')(decoder2)
+
+    # tie it together [image, seq] [word]
+    model = Model(inputs=[inputs1, inputs2], outputs=outputs)
+    model.compile(loss='categorical_crossentropy', optimizer='adam')
+
+    # summarize model
+    print(model.summary())
+    plot_model(model, to_file='src/model.png', show_shapes=True)
+
+    return model
