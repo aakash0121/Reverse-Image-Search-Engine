ca980e9b0ad3582b5a7a3d0e8d7ffbe61a8bf8c7
Aakash Bindal <bindalaakash60@gmail.com>
2020-05-08 15:53:15 +0530

    load_dataset

---
+++ load_dataset_for_train.py
@@ -0,0 +1,39 @@
+from description_generator import load_doc, dataset_text
+from pickle import dump, load
+
+
+#load the data 
+def load_photos(filename):
+    file = load_doc(filename)
+    photos = file.split("\n")[:-1]
+    return photos
+
+def load_clean_descriptions(filename, photos): 
+    #loading clean_descriptions
+    file = load_doc(filename)
+    descriptions = {}
+    for line in file.split("\n"):
+        words = line.split()
+        if len(words)<1 :
+            continue
+        image, image_caption = words[0], words[1:]
+        if image in photos:
+            if image not in descriptions:
+                descriptions[image] = []
+            desc = '<start> ' + " ".join(image_caption) + ' <end>'
+            descriptions[image].append(desc)
+    return descriptions
+
+def load_features(photos):
+    #loading all features
+    all_features = load(open("src/features.p","rb"))
+    #selecting only needed features
+    features = {k:all_features[k] for k in photos}
+    return features
+
+
+filename = dataset_text + "/" + "Flickr_8k.trainImages.txt"
+#train = loading_data(filename)
+train_imgs = load_photos(filename)
+train_descriptions = load_clean_descriptions("src/descriptions.txt", train_imgs)
+train_features = load_features(train_imgs)
